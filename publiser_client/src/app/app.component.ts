716348eac692b14921c963e25919bd98b57607f3
Jairosz <Jairosg03@gmail.com>
2024-07-16 22:20:50 +0200

    servicios
    servicios

--- publiser_client/src/app/app.component.ts
+++ publiser_client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LandingComponent],
+  imports: [RouterOutlet, NavbarComponent, LandingComponent, ServiciosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
