e280e9dec80ef6d7a091de1e17aae07b67eaa711
Jairosz <Jairosg03@gmail.com>
2024-08-07 10:09:14 +0200

    a
    a

--- publiser_client/src/app/app.routes.ts
+++ publiser_client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+];
